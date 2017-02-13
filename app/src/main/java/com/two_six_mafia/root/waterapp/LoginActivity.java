9ca0b5d86b34434a186805e4a4fde63ef5ca5ea6
aaron <aorquia3@gatech.edu>
2017-02-12 19:21:34 -0500

    Added HomescreenActivity

--- app/src/main/java/com/two_six_mafia/root/waterapp/LoginActivity.java
+++ app/src/main/java/com/two_six_mafia/root/waterapp/LoginActivity.java
@@ -82,8 +82,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button mSignInButton = (Button) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
