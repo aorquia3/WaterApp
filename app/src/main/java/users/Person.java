30f334fbeb03a9e8a26d6453df72d250fd93e336
aaron <aorquia3@gatech.edu>
2017-02-22 22:19:02 -0500

    Added userType enum

--- app/src/main/java/users/Person.java
+++ app/src/main/java/users/Person.java
@@ -8,19 +8,26 @@ public class Person {
     private final String username;
     //For now, passwords are still stored in plain text.
     private String password;
+    private String name;
+    private UserType userType;
 
     public Person() {
         this.username = null;
         this.password = null;
     }
 
-    public Person(String username, String password) {
+    public Person(String username, String password, String name) {
         this.username = username;
         this.password = password;
+        this.name = name;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public boolean checkPassword(String input) {
@@ -30,4 +37,6 @@ public class Person {
     public void updatePassword(String password) {
         this.password = password;
     }
+
+    public String toString()
 }
---
+++ app/src/main/java/users/UserType.java
@@ -0,0 +1,9 @@
+package users;
+
+/**
+ * Created by aaron on 2/22/17.
+ */
+
+public enum UserType {
+    USER, WORKER, MANAGER, ADMIN
+}
