055fb666cde67bcf8c1ee4c1ee7baed3e1c1125d
rebekahjaramillo <rebekah.jara@gmail.com>
2017-03-02 00:54:44 -0500

    Source Report Water Condition & Type

--- app/src/main/java/com/two_six_mafia/root/waterapp/Controller/SourceReportActivity.java
+++ app/src/main/java/com/two_six_mafia/root/waterapp/Controller/SourceReportActivity.java
@@ -2,23 +2,45 @@ package com.two_six_mafia.root.waterapp.Controller;
 
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.two_six_mafia.root.waterapp.R;
 
 import java.util.Locale;
 
 public class SourceReportActivity extends AppCompatActivity {
 
+    private Spinner waterType;
+    private Spinner waterCondition;
+    private EditText userName;
+    private EditText sourceLocation;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        waterType = (Spinner) findViewById(R.id.waterType);
+        waterCondition = (Spinner) findViewById(R.id.waterCondition);
+        userName = (EditText) findViewById(R.id.userName);
+        sourceLocation = (EditText) findViewById(R.id.sourceLocation);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_source_report);
 
-        EditText autoDate = (EditText)findViewById(R.id.autoDate);
-        EditText autoTime = (EditText)findViewById(R.id.autoTime);
+        EditText autoDate = (EditText) findViewById(R.id.autoDate);
+        EditText autoTime = (EditText) findViewById(R.id.autoTime);
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("d MMM yyyy", Locale.getDefault());
         SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
@@ -28,5 +50,42 @@ public class SourceReportActivity extends AppCompatActivity {
 
         autoDate.setText(date);
         autoTime.setText(time);
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("SourceReport Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
