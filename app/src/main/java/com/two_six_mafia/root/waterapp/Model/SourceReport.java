59144f774b42e398c02721507d92b6e6de8f873c
aaron <aorquia3@gatech.edu>
2017-03-01 22:32:54 -0500

    Add files for intial source report submission.

---
+++ app/src/main/java/com/two_six_mafia/root/waterapp/Model/SourceReport.java
@@ -0,0 +1,13 @@
+package com.two_six_mafia.root.waterapp.Model;
+
+/**
+ * Created by aaron on 3/1/17.
+ * Used with the Water Source class to track initial water reports.
+ */
+
+public class SourceReport {
+    private static int REPORT_NUMBER;
+    private int date;
+    private String reporter;
+    private WaterSource source;
+}
---
+++ app/src/main/java/com/two_six_mafia/root/waterapp/Model/WaterSource.java
@@ -0,0 +1,9 @@
+package com.two_six_mafia.root.waterapp.Model;
+
+/**
+ * Created by aaron on 3/1/17.
+ */
+
+public class WaterSource {
+    private SourceReport report;
+}
