880a5cc1bbcd402d45724bfaa81165c2f05bbf70
Claire Monsour <cmonsour3@gatech.edu>
2017-04-25 09:03:57 -0400

    iOS App should work now or I'll kill someone. P.S. Xcode sucks

--- WaterAppiOSVersion/Drop/AppDelegate.swift
+++ WaterAppiOSVersion/Drop/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import GoogleMaps
+//import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyBDcDaSH3z-l5J1OV7DLfFnnMucEvRAdHU")
+        //GMSServices.provideAPIKey("AIzaSyBDcDaSH3z-l5J1OV7DLfFnnMucEvRAdHU")
         return true
     }
 
--- WaterAppiOSVersion/Drop/WaterAvailabilityViewController.swift
+++ WaterAppiOSVersion/Drop/WaterAvailabilityViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import GoogleMaps
+//import GoogleMaps
 
 class WaterAvailabilityViewController: UIViewController {
 
@@ -21,20 +21,20 @@ class WaterAvailabilityViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func loadView() {
+    //override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
+        //let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        //let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        //view = mapView
         
         // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-    }
+        //let marker = GMSMarker()
+        //marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        //marker.title = "Sydney"
+        //marker.snippet = "Australia"
+        //marker.map = mapView
+    //}
 
     /*
     // MARK: - Navigation
